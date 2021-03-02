b93f3a873622f15f3d97216dbb71294b4a66f186
Umnov Stanislav <umnov.msk@gmail.com>
2021-03-02 14:56:46 +0300

    Добавлен обработчик ошибок

--- Program.cs
+++ Program.cs
@@ -11,58 +11,64 @@ namespace AutoPrice
         {
             try
             {
-                while (true)
-                {
-                    // Настраиваем таймер запуска
-                    var timeNow = DateTime.Now;
-                    var timeToRun = new DateTime(timeNow.Year, timeNow.Month, timeNow.Day + 1, 6, 30, 0);
-                    var delayTime = timeToRun - timeNow;
-                    Task.Delay(delayTime).Wait();
-                    // ==========================
-
-                    var errorLog = new ErrorLogging();
-                    var config = new Config(errorLog);
-                    var addInfo = new AdditionalInfo(config);
-                    var priceModel = new PriceModel(config);
-                    var priceList = new PriceList(config, errorLog);
-                    var makeExcel = new MakeExcelFile(config, errorLog);
-                    var makeArchive = new MakeArchiveFile(config, errorLog);
-                    var upload = new UploadToFTP(config, errorLog);
-                    var remove = new RemoveOldPrice(config, errorLog);
-                    var report = new EmailReport(config, errorLog);
-
-                    if (!config.GetConfig())
-                    {
-                        return;
-                    }
-
-                    try
-                    {
-                        var price = priceList.MakePriceList();
-                        makeExcel.SavePriceAsExcel(price);
-                        makeArchive.CreateArchiveFile();
-                        upload.UploadPrice();
-                        remove.Remove();
-                    }
-                    catch (Exception ex)
-                    {
-                        errorLog.ErrorMessage = $"{DateTime.Now} : При поптыке сформировать прайс-лист произошла ошибка \n{ex}";
-                    }
-
-                    try
-                    {
-                        report.SendReport();
-                    }
-                    catch (Exception ex)
-                    {
-                        errorLog.ErrorMessage = $"{DateTime.Now} : При поптыке отправить письма произошла ошибка \n{ex}";
-                    }
-                }
+                RunAutoPrice();
             }
             catch (Exception ex) 
             {
-                var error = new ErrorLogging();
-                error.ErrorMessage = $"{DateTime.Now} : Произошла глобальная ошибка, для которой не был предусмотрен обработчик ошибок. \n{ex}";
+                var errorLog = new ErrorLogging();
+                errorLog.ErrorMessage = $"{DateTime.Now} : Произошла глобальная ошибка, для которой не был предусмотрен обработчик ошибок. \n{ex}";
+                RunAutoPrice();
+            }
+        }
+
+        private static void RunAutoPrice()
+        {
+            while (true)
+            {
+                // Настраиваем таймер запуска
+                var timeNow = DateTime.Now;
+                var timeToRun = new DateTime(timeNow.Year, timeNow.Month, timeNow.Day + 1, 6, 30, 0);
+                var delayTime = timeToRun - timeNow;
+                Task.Delay(delayTime).Wait();
+                // ==========================
+
+                var errorLog = new ErrorLogging();
+                var config = new Config(errorLog);
+                var addInfo = new AdditionalInfo(config);
+                var priceModel = new PriceModel(config);
+                var priceList = new PriceList(config, errorLog);
+                var makeExcel = new MakeExcelFile(config, errorLog);
+                var makeArchive = new MakeArchiveFile(config, errorLog);
+                var upload = new UploadToFTP(config, errorLog);
+                var remove = new RemoveOldPrice(config, errorLog);
+                var report = new EmailReport(config, errorLog);
+
+                if (!config.GetConfig())
+                {
+                    return;
+                }
+
+                try
+                {
+                    var price = priceList.MakePriceList();
+                    makeExcel.SavePriceAsExcel(price);
+                    makeArchive.CreateArchiveFile();
+                    upload.UploadPrice();
+                    remove.Remove();
+                }
+                catch (Exception ex)
+                {
+                    errorLog.ErrorMessage = $"{DateTime.Now} : При поптыке сформировать прайс-лист произошла ошибка \n{ex}";
+                }
+
+                try
+                {
+                    report.SendReport();
+                }
+                catch (Exception ex)
+                {
+                    errorLog.ErrorMessage = $"{DateTime.Now} : При поптыке отправить письма произошла ошибка \n{ex}";
+                }
             }
         }
     }
